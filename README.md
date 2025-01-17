8727af371c42c20905f6ee75b046281c1f1e4cee
Ibraheem <highfrezh14@gmail.com>
2025-01-17 06:30:31 +0000

    update file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Revisiting-Javascript
+# Start your year 2025
+
+deno run --check typescript/enhanced-knowledge/interfaces.ts deno fmt deno run
+--check typescript/challenges/part1.ts
--- typescript/challenges/part1.ts
+++ typescript/challenges/part1.ts
@@ -1,15 +1,12 @@
-function plus(x: number | string, y: number | string): number | string | undefined {
-    if (typeof x === "number" && typeof y === "number") {
-      return x + y;  
-    }
-   
-    else if (typeof x === "string" && typeof y === "string") {
-      return x + y;  // Concatenate the strings if both are strings
-    }
-    return undefined;
-  }
-  
-  console.log(plus(2025, 1));            
-  console.log(plus(undefined as any, 21));
-  console.log(plus("StackUp", "Learn"));
-  
+function plus(
+  x: number | string | undefined, 
+  y: number | string | undefined,
+){
+  if ((typeof x === "number") && (typeof y === "number")) return x + y;
+  if ((typeof x === "string") && (typeof y === "string")) return x + y;
+  else return undefined;
+}
+
+console.log(plus(2025, 1));
+console.log(plus(undefined, 21));
+console.log(plus("StackUp", "Learn"));
